a2b74c247816b20058deded6943ef1f4315dd72c
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-09-02 16:38:52 +0900

    コントローラー名をitemsに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'tops#index'
-  resources :tops, only: [:new, :create]
+  root to: 'items#index'
+  resources :items, only: [:new, :create]
 end
