b02746aace43798de43035fbe6dc621b9f230932
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-09-03 18:14:41 +0900

    未ログイン時に投稿しようとするとログイン画面に遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sign_in, except: [:index,]
+
   def index
   end
 
@@ -12,6 +14,12 @@ class ItemsController < ApplicationController
 
  private
   def item_params
-    params.require(:item).permit(:name, :category, :image, :text, :status, :burden, :shipping_origin, :shipping_day, :price)
+    params.require(:item,).permit(:name, :category_id, :image, :text, :status_id, :burden_id, :shipping_origin_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to "/users/sign_in"
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :status
+  belongs_to_active_hash :category
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :shipping_origin
+  belongs_to_active_hash :shipping_day
+
 
   # バリデーション
   validates :name, presence: true
@@ -10,5 +17,12 @@ class Item < ApplicationRecord
   validates :burden, presence: true
   validates :shipping_origin, presence: true
   validates :shipping_day, presence: true
-  validates :price, presence: true
+  validates :price, presence: true,
+            format: { with: /\A[0-9]+\z/, message: "Password Include both letters and numbers" }
+  #一つ目の選択「--」の時は保存できないようにする
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :burden_id, numericality: { other_than: 1 } 
+  validates :shipping_origin_id, numericality: { other_than: 1 } 
+  validates :shipping_day_id, numericality: { other_than: 1 } 
 end
