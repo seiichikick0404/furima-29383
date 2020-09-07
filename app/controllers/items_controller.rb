a2c6a904e0d7984a9f8bdeb661338d48adb458ac
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-09-07 19:46:37 +0900

    updateの成功時と失敗時の処理の分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,9 +27,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render action: :edit
+    end
   end
 
  private
