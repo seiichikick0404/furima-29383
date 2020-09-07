1133fe979dcd943ecaafd047492bf91240a4a27c
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-09-07 16:46:19 +0900

    editコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
  private
   def item_params
     params.require(:item).permit(:name, :category_id, :image, :text, :status_id, :burden_id, :shipping_origin_id, :shipping_day_id, :price).merge(user_id: current_user.id)
