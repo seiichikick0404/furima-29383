3df88ef17cb6ba7effd87f74a3d51aa6cea1859f
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-09-16 18:56:17 +0900

    削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,6 +7,15 @@ class MessagesController < ApplicationController
     end
   end
 
+  def destroy
+    @message = Message.find(params[:id])
+    if @message.destroy
+      redirect_to "/items/#{@message.item.id}"
+    else
+      render 'items/show'
+    end
+  end
+
   private
   def message_params
     params.require(:message).permit(:comments).merge(user_id: current_user.id, item_id: params[:item_id])
